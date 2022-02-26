23c34cf819e6d830ad78fcb45c9c921e86259788
lucasbernardol <jose.lukass03@gmail.com>
2022-02-26 16:28:05 -0300

    chore: Add TypeORM config file

---
+++ ormconfig.js
@@ -0,0 +1,31 @@
+require('dotenv/config');
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+const folder = isDevelopment ? 'src' : 'dist';
+
+const typeORMExtrasOptions = {
+  logging: true,
+  synchronize: true,
+};
+
+module.exports = {
+  ...typeORMExtrasOptions,
+
+  type: process.env.DATABASE_TYPE || 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT) || 5432,
+
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+
+  entities: [`${folder}/api/core/entities/*{.ts,.js}`],
+
+  migrationsTableName: 'migrations',
+  migrations: [`${folder}/api/core/database/migrations/*{.ts,.js}`],
+
+  cli: {
+    migrationsDir: `${folder}/api/core/database/migrations`,
+  },
+};
